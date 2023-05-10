be6630cc445fdfc8abbf145199b6b4a09b798db5
Toni <antonia-saile@gmx.de>
2023-05-10 22:39:48 +0200

    Refactor day 1

--- D01.py
+++ D01.py
@@ -1,8 +1,10 @@
 with open("D01.txt") as file:
-    input = list(map(int,file.read().splitlines()))
+    input = list(map(int, file.read().splitlines()))
+
 
 def Day01(inp):
-  return len(list(filter(lambda x: x[0]<x[1],inp)))
+    return len(list(filter(lambda x: x[0] < x[1], inp)))
+
 
 print(Day01(zip(input, input[1:])))
 print(Day01(zip(input, input[3:])))
