a08821e202c4de432220a48161cc4dd8b24e58aa
Toni <antonia-saile@gmx.de>
2023-05-10 22:36:38 +0200

    Add 2021 day 1

---
+++ D01.py
@@ -0,0 +1,8 @@
+with open("D01.txt") as file:
+    input = list(map(int,file.read().splitlines()))
+
+def Day01(inp):
+  return len(list(filter(lambda x: x[0]<x[1],inp)))
+
+print(Day01(zip(input, input[1:])))
+print(Day01(zip(input, input[3:])))
